e5001193852e74b2cad797f643021090c4292384
ciscoo <cisco21c@gmail.com>
2018-05-20 13:06:21 -0500

    Add unit tests for GoogleDriveController

---
+++ src/test/java/io/mateo/googleapisdemo/controller/GoogleDriveControllerTest.java
@@ -0,0 +1,40 @@
+package io.mateo.googleapisdemo.controller;
+
+import com.google.api.services.drive.model.File;
+import io.mateo.googleapisdemo.service.GoogleDriveService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class GoogleDriveControllerTest {
+    private GoogleDriveController googleDriveController;
+
+    @Mock
+    private GoogleDriveService googleDriveService;
+
+    @Before
+    public void setUp() {
+        googleDriveController = new GoogleDriveController(googleDriveService);
+    }
+
+    @Test
+    public void shouldGetFileInfo() throws IOException {
+        when(googleDriveService.getFileMetaData(anyString())).thenReturn(new File());
+        assertThat(googleDriveController.getFileInfo("")).isInstanceOf(File.class);
+    }
+
+    @Test
+    public void shouldGetRawFile() throws IOException {
+        when(googleDriveService.getFile(anyString())).thenReturn(new byte[] {});
+        assertThat(googleDriveController.getFile("")).isInstanceOf(byte[].class);
+    }
+}
